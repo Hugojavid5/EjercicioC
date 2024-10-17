b9cb51c9350e7dd5e4814a8da358ac259250dfeb
Hugo <hugojavidferrero@gmail.com>
2024-10-17 14:03:50 +0200

    Ejercicio a medias para acabar en casa

--- src/main/java/org/hugo/ejercicioc/PersonaController.java
+++ src/main/java/org/hugo/ejercicioc/PersonaController.java
@@ -1,7 +1,9 @@
 package org.hugo.ejercicioc;
 
+import Model.Persona;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -30,23 +32,155 @@ public class PersonaController {
     private TextField txtNombre;
 
     @FXML
-    void accionCargarDatos(MouseEvent event) {
-
+    void mostrarError(String error)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setTitle("Error");
+        alert.setContentText(error);
+        alert.showAndWait();
     }
 
     @FXML
-    void ag_persona(ActionEvent event) {
-
+    void mostrarInfo(String info)
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Info");
+        alert.setContentText(info);
+        alert.showAndWait();
     }
 
     @FXML
-    void eliminar(ActionEvent event) {
-
+    void limpiarDatos()
+    {
+        txtNombre.setText("");
+        txtApellidos.setText("");
+        txtEdad.setText("");
+        tablaPersonas.getSelectionModel().clearSelection();
+    }
+    @FXML
+    void cargarDatos(MouseEvent event)
+    {
+        Persona p = tablaPersonas.getSelectionModel().getSelectedItem();
+        if (p!=null){
+            txtNombre.setText(p.getNombre());
+            txtApellidos.setText(p.getApellidos());
+            txtEdad.setText(p.getEdad()+"");
+        }
     }
 
     @FXML
-    void modificar(ActionEvent event) {
+    void ag_persona(ActionEvent event)
+    {
+            String error="";
+            if (txtNombre.getText().isEmpty()){
+                error+="Introduce un Nombre\n";
+            }
+            if (txtApellidos.getText().isEmpty()){
+                error+="Introduce un Apellido\n";
+            }
+            if (txtEdad.getText().isEmpty()){
+                error+="Introduce una Edad\n";
+            }else{
+                try {
+                    int edad=Integer.parseInt(txtEdad.getText());
+                    if(edad<=0) {
+                        throw new Exception();
+                    }
+                } catch (NumberFormatException e) {
+                    error+="Introduce un entero como Edad\n";
+                } catch (Exception e) {
+                    error+="Introduce un número superior a 0\n";
+                }
+            }
+            if (error.isEmpty()){
+                Boolean existe=false;
+                Persona p =new Persona(txtNombre.getText(),txtApellidos.getText(),Integer.parseInt(txtEdad.getText()));
+                for (Persona persona:tablaPersonas.getItems()){
+                    if (persona.equals(p)){
+                        mostrarError("Esa persona ya existe en la tabla");
+                        existe=true;
+                        break;
+                    }
+                }
+                if (!existe){
+                    tablaPersonas.getItems().add(p);
+                    limpiarDatos();
+                    mostrarInfo("Persona añadida correctamente");
+                }
+            }else {
+                mostrarError(error);
+            }
+    }
 
+    @FXML
+    void eliminar(ActionEvent event)
+    {
+        Persona p=tablaPersonas.getSelectionModel().getSelectedItem();
+        if (p==null){
+            mostrarError("Selecciona una persona de la tabla");
+        }else{
+            tablaPersonas.getItems().remove(p);
+            limpiarDatos();
+            mostrarInfo("Persona eliminada correctamente");
+        }
+    }
+
+    @FXML
+    void modificar(ActionEvent event)
+    {
+        Persona personaSeleccionada = tablaPersonas.getSelectionModel().getSelectedItem();
+        if (personaSeleccionada==null){
+            mostrarError("Selecciona una persona de la tabla");
+        }else {
+            //Comprobar Datos
+            String error="";
+            if (txtNombre.getText().isEmpty()){
+                error+="Introduce un Nombre\n";
+            }
+            if (txtApellidos.getText().isEmpty()){
+                error+="Introduce un Apellido\n";
+            }
+            if (txtEdad.getText().isEmpty()){
+                error+="Introduce una Edad\n";
+            }else{
+                try {
+                    int edad=Integer.parseInt(txtEdad.getText());
+                    if(edad<=0) {
+                        throw new Exception();
+                    }
+                } catch (NumberFormatException e) {
+                    error+="Introduce un entero como Edad\n";
+                } catch (Exception e) {
+                    error+="Introduce un número superior a 0\n";
+                }
+            }
+
+            if (error.isEmpty()){
+                //Comprobar si existe
+                Boolean existe=false;
+                Persona p =new Persona(txtNombre.getText(),txtApellidos.getText(),Integer.parseInt(txtEdad.getText()));
+                for (Persona persona:tablaPersonas.getItems()){
+                    if (persona.equals(p)){
+                        mostrarError("Esa persona ya existe en la tabla");
+                        existe=true;
+                        break;
+                    }
+                }
+                if (!existe){
+                    // Actualizar los datos de la persona seleccionada
+                    personaSeleccionada.setNombre(txtNombre.getText());
+                    personaSeleccionada.setApellidos(txtApellidos.getText());
+                    personaSeleccionada.setEdad(Integer.parseInt(txtEdad.getText()));
+                    tablaPersonas.refresh();
+                    limpiarDatos();
+                    mostrarInfo("Persona modificada correctamente");
+                }
+            }else {
+                mostrarError(error);
+            }
+        }
     }
 
 }
