3c8e7597b03cf1d161f3eb074d145d42d36f8090
Hugo <hugojavidferrero@gmail.com>
2024-10-17 13:42:30 +0200

    Clase HelloApplication sin acabar

--- src/main/java/org/hugo/ejercicioc/HelloApplication.java
+++ src/main/java/org/hugo/ejercicioc/HelloApplication.java
@@ -3,6 +3,7 @@ package org.hugo.ejercicioc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioC.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Image icon = new Image(getClass().getResourceAsStream("/Imagenes/icono.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Personas");
+        stage.setMinWidth(600);
+        stage.setMinHeight(400);
         stage.setScene(scene);
         stage.show();
     }
