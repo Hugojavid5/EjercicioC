b5371887f592821c5d5f12c6d376804b535ddadb
Hugo <hugojavidferrero@gmail.com>
2024-10-21 10:38:03 +0200

    Hello aplication terminada con javadocs

--- src/main/java/org/hugo/ejercicioc/HelloApplication.java
+++ src/main/java/org/hugo/ejercicioc/HelloApplication.java
@@ -8,7 +8,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * La clase HelloApplication es la aplicación principal que inicia la interfaz gráfica de usuario (GUI).
+ * Extiende la clase Application de JavaFX.
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * El método start es el punto de entrada de la aplicación.
+     * @param stage El escenario principal donde se mostrará la escena.
+     * @throws IOException Si ocurre un error al cargar el archivo FXML.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioC.fxml"));
@@ -22,6 +32,10 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    /**
+     * El método main es el punto de entrada de la aplicación.
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         launch();
     }
