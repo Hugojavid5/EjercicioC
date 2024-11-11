5343faa09a18d1a8256c6fb8f45e1eb3c98fe652
Hugo <hugojavidferrero@gmail.com>
2024-11-11 11:41:40 +0100

    ejercicio corregido

--- src/main/java/org/hugo/ejercicioc/AppPersonas.java
+++ src/main/java/org/hugo/ejercicioc/AppPersonas.java
@@ -12,20 +12,21 @@ import java.io.IOException;
  * La clase HelloApplication es la aplicación principal que inicia la interfaz gráfica de usuario (GUI).
  * Extiende la clase Application de JavaFX.
  */
-public class HelloApplication extends Application {
+public class AppPersonas extends Application {
 
     /**
-     * El método start es el punto de entrada de la aplicación.
+     * El metodo start es el punto de entrada de la aplicación.
      * @param stage El escenario principal donde se mostrará la escena.
      * @throws IOException Si ocurre un error al cargar el archivo FXML.
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EjercicioC.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppPersonas.class.getResource("EjercicioC.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Image icon = new Image(getClass().getResourceAsStream("/Imagenes/icono.png"));
         stage.getIcons().add(icon);
         stage.setTitle("Personas");
+        stage.setResizable(false);
         stage.setMinWidth(600);
         stage.setMinHeight(400);
         stage.setScene(scene);
@@ -33,7 +34,7 @@ public class HelloApplication extends Application {
     }
 
     /**
-     * El método main es el punto de entrada de la aplicación.
+     * El metodo main es el punto de entrada de la aplicación.
      * @param args Argumentos de la línea de comandos.
      */
     public static void main(String[] args) {
